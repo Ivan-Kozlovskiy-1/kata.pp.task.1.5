fe6a6ec57270a34d6eca25944661ac533984cedc
Ivan <onees5976258@gmail.com>
2023-11-21 01:07:07 +0300

    solved task 1.5+

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,87 +8,88 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    Connection connection = Util.getConnection();
+    //Connection connection = Util.getConnection();
 
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        try (Statement statement = connection.createStatement()) {
-            statement.execute("CREATE TABLE IF NOT EXISTS task (id INT NOT NULL AUTO_INCREMENT " +
-                    "PRIMARY KEY,name VARCHAR(99) NOT NULL,last_name VARCHAR(99) NOT NULL, age INT);");
-            connection.commit();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+//        try (Statement statement = connection.createStatement()) {
+//            statement.execute("CREATE TABLE IF NOT EXISTS task (id INT NOT NULL AUTO_INCREMENT " +
+//                    "PRIMARY KEY,name VARCHAR(99) NOT NULL,last_name VARCHAR(99) NOT NULL, age INT);");
+//            connection.commit();
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
 
     }
 
 
     public void dropUsersTable() {
-        String drop = "DROP TABLE IF EXISTS task";
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate(drop);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
+//        String drop = "DROP TABLE IF EXISTS task";
+//        try (Statement statement = connection.createStatement()) {
+//            statement.executeUpdate(drop);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//
     }
 
 
     public void saveUser(String name, String lastName, byte age) {
-        try (PreparedStatement prepareStatement = connection.prepareStatement("INSERT INTO task (name, last_name, age) VALUES  (?, ?, ?);")) {
-            prepareStatement.setString(1, name);
-            prepareStatement.setString(2, lastName);
-            prepareStatement.setByte(3, age);
-            prepareStatement.executeUpdate();
-            System.out.println("User named - " + name + " added in database");
-
-            connection.commit();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+//        try (PreparedStatement prepareStatement = connection.prepareStatement("INSERT INTO task (name, last_name, age) VALUES  (?, ?, ?);")) {
+//            prepareStatement.setString(1, name);
+//            prepareStatement.setString(2, lastName);
+//            prepareStatement.setByte(3, age);
+//            prepareStatement.executeUpdate();
+//            System.out.println("User named - " + name + " added in database");
+//
+//            connection.commit();
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
 
     }
 
 
     public void removeUserById(long id) {
-        String sqlQuery = "DELETE FROM task WHERE id = ?";
-        try (PreparedStatement pStat = connection.prepareStatement(sqlQuery)) {
-            pStat.setString(1, String.valueOf(id));
-            pStat.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+//        String sqlQuery = "DELETE FROM task WHERE id = ?";
+//        try (PreparedStatement pStat = connection.prepareStatement(sqlQuery)) {
+//            pStat.setString(1, String.valueOf(id));
+//            pStat.executeUpdate();
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
 
     }
 
 
     public List<User> getAllUsers() {
-        List<User> list = new ArrayList<>();
-        try (Statement statement = connection.createStatement()) {
-            ResultSet rs = statement.executeQuery("SELECT * FROM task;");
-            while (rs.next()) {
-                list.add(new User(rs.getString("name"), rs.getString("last_name"),
-                        rs.getByte("age")));
-            }
-            connection.commit();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return list;
+//        List<User> list = new ArrayList<>();
+//        try (Statement statement = connection.createStatement()) {
+//            ResultSet rs = statement.executeQuery("SELECT * FROM task;");
+//            while (rs.next()) {
+//                list.add(new User(rs.getString("name"), rs.getString("last_name"),
+//                        rs.getByte("age")));
+//            }
+//            connection.commit();
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return list;
+        return null;
     }
 
     public void cleanUsersTable() {
-        String sqlQuery = "DELETE FROM task";
-        try (Statement stat = connection.createStatement()) {
-            stat.executeUpdate(sqlQuery);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+//        String sqlQuery = "DELETE FROM task";
+//        try (Statement stat = connection.createStatement()) {
+//            stat.executeUpdate(sqlQuery);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,25 +1,40 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
+        import jm.task.core.jdbc.model.User;
+        import org.hibernate.HibernateException;
+        import org.hibernate.SessionFactory;
+        import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+        import org.hibernate.cfg.Configuration;
+        import org.hibernate.service.ServiceRegistry;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/test";
     private static final String LOGIN = "root";
     private static final String PASSWORD = "Sqnpfm2b0561";
+    private static SessionFactory sessionFactory;
 
+    public static SessionFactory getConnection() {
 
-    public static Connection getConnection () {
         try {
-            Connection connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
-            connection.setAutoCommit(false);
-            return connection;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.driver_class", DRIVER)
+                    .setProperty("hibernate.connection.url", URL)
+                    .setProperty("hibernate.connection.username", LOGIN)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .addAnnotatedClass(User.class)
+                    .setProperty("hibernate.c3p0.min_size","9")
+                    .setProperty("hibernate.c3p0.max_size","999")
+                    .setProperty("hibernate.c3p0.max_statements","999");
 
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
     }
 }
+
